0d862396cc57c65978e59e1bbb66c9f5cf2251c8
DuelQ <DuelQ@wp.pl>
2016-11-19 17:03:49 +0100

    Add files via upload

---
+++ nauka1.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <cstdio>
+using namespace std;
+int main (void)
+{    int a;
+      cout<<"Podaj a:";
+      cin>>a;
+     int b;
+      cout<<"Podaj b";
+      cin>>b;
+    
+     cout<<"a + b= "<< a + b<<endl;
+     cout<<"a - b= "<< a- b<<endl;
+     cout<<"a * b= "<< a* b<<endl;
+     cout<<"a % b= "<< a% b<<endl;
+    return 0;
+    }
